74c2b378bc30b02c168f4e722f440fee83856d85
DanJeEpic <98715915+DanJeEpicMan@users.noreply.github.com>
2022-05-19 15:58:03 -0400

    Add files via upload

---
+++ lukes autoclick/install.py
@@ -0,0 +1,10 @@
+import os
+import time
+print("installing... (this will take around 3 min)")
+time.sleep(1.5)
+print("dissrigard yellow text")
+time.sleep(1.5)
+os.system('pip install keyboard')
+os.system('pip install pipwin')
+os.system('pipwin install SpeechRecognition')
+os.system('python main.py')
